561eb7971567909a2a33efa6f1e18fb54a618a35
Eugene <prokhorov.eu@icloud.com>
2020-03-18 11:25:10 +0400

    Initial Commit

---
+++ SGU/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SGU
+//
+//  Created by Евгений Прохоров on 18.03.2020.
+//  Copyright © 2020 Евгений Прохоров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
